2b4e92535d813161944611bb31df21556feda72d
Kevin Ngo <66854579+KennyKnows@users.noreply.github.com>
2023-11-21 15:32:09 -0500

    Add files via upload

---
+++ TCP Port Scanner (80, 443, 3389).ps1
@@ -0,0 +1,12 @@
+﻿$hostname = "192.168.1.1" #Replace with the IP address or hostname you want to scan.
+$ports = 80, 443, 3389 #Replace with the ports you want to scan.
+
+foreach ($port in $ports) {
+    $result = Test-NetConnection -ComputerName $hostname -Port $port -InformationLevel "Detailed"
+    
+    if ($result.TcpTestPass) {
+        Write-Host "Port $port is open"
+    } else {
+        Write-Host "Port $port is closed"
+    }
+}
